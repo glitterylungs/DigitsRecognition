ec06da2adf8177210739734704b1a741eed667fb
Alicja Gruca <alicjagruca6@gmail.com>
2022-08-16 23:45:46 +0200

    Initial Commit

---
+++ DigitsRecognition/DigitsRecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigitsRecognitionApp.swift
+//  DigitsRecognition
+//
+//  Created by Alicja Gruca on 16/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct DigitsRecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
